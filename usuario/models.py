48b41ecce13bb76781740aeededd10082f38baec
xavier <xavier.bonillaplaza@gmail.com>
2021-01-20 10:58:14 -0300

    Mi Proyecto Django

---
+++ usuario/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from django.db import models
+from django.utils import timezone
+
+class Persona(models.Model):
+    rut = models.CharField(max_length=12, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    email = models.EmailField()
+    fecha_creado = models.DateTimeField(blank=True, null=True)
+
+    def creacion(self):
+        self.fecha_creado = timezone.now()
+        self.save()
+
